bef8461708fa7bb8e2e790ff229efb4ff7e5a8f8
rrworkscom <rrworks010@gmail.com>
2026-04-09 09:16:34 +0700

    Dynamic Update

--- week05/StudentDemo20.java
+++ week05/StudentDemo20.java
@@ -1,21 +1,39 @@
 package week05;
 
+import java.util.Scanner;
+
 public class StudentDemo20 {
     public static void main(String[] args) {
-        TopStudent20 topStudents = new TopStudent20(5);
+        Scanner sc = new Scanner(System.in);
+        String nim, name, studentClass;
+        double gpa; 
 
-        topStudents.add(new Student20("2201", "Alice", "A", 3.9 ));
-        topStudents.add(new Student20("2202", "Bob", "B", 3.7));
-        topStudents.add(new Student20("2203", "Charlie", "C", 3.8));
-        topStudents.add(new Student20("2204", "David", "D", 3.6));
-        topStudents.add(new Student20("2205", "Eve", "E", 4.0));
+        System.out.print("Input data size: ");
+        int size = Integer.parseInt(sc.nextLine());
+
+        TopStudent20 topStudents = new TopStudent20(size);
+
+        for (int i = 0; i < size; i++) {
+        System.out.println("Input data for student #" + (i + 1));
+        System.out.print("NIM: ");
+        nim =  sc.nextLine();
+        System.out.print("Name: ");
+        name = sc.nextLine();
+        System.out.print("Student's class: ");
+        studentClass = sc.nextLine();
+        System.out.print("GPA: ");
+        gpa = Double.parseDouble(sc.nextLine());
+
+        topStudents.add(new Student20(nim, name, studentClass, gpa));
+        }
 
         System.out.println("Original student list: ");
         topStudents.print();
 
-        topStudents.bubbleSort();
-        System.out.println("Sorted student list (by GPA, descending:");
+        topStudents.insertionSort();
+        System.out.println("Sorted student list (by GPA, ascending) using Insertion Sort:");
         topStudents.print();
+        
 
     }
 }
